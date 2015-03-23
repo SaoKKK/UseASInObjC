c50fe0bf304f6d825f74b6f5ddcaa113a1a47aa2
Saori Kohno <SaoKKK@users.noreply.github.com>
2015-03-23 17:22:31 +0900

    Initial Commit

---
+++ UseASInObjC/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  UseASInObjC
+//
+//  Created by 河野 さおり on 2015/03/23.
+//  Copyright (c) 2015年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ UseASInObjCTests/UseASInObjCTests.m
@@ -0,0 +1,40 @@
+//
+//  UseASInObjCTests.m
+//  UseASInObjCTests
+//
+//  Created by 河野 さおり on 2015/03/23.
+//  Copyright (c) 2015年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface UseASInObjCTests : XCTestCase
+
+@end
+
+@implementation UseASInObjCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
