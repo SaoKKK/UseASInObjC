16a81cd23c8acef6e687ea9eada4f53d1a0c4ed9
Saori Kohno <SaoKKK@users.noreply.github.com>
2015-03-23 22:21:32 +0900

    Completion

--- UseASInObjC/AppDelegate.m
+++ UseASInObjC/AppDelegate.m
@@ -10,7 +10,6 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
 @end
 
 @implementation AppDelegate
@@ -21,6 +20,61 @@
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (NSAppleEventDescriptor*)GetAEDesc:(NSString*)ASName{
+    NSDictionary *asErrDic;
+    NSString *asPath = [[[NSBundle mainBundle]resourcePath]stringByAppendingPathComponent:ASName];
+    NSURL *url = [NSURL fileURLWithPath:asPath];
+    NSAppleScript *as = [[NSAppleScript alloc]initWithContentsOfURL:url error:&asErrDic];
+    
+    if (asErrDic) {
+        //エラー処理
+    }
+    NSLog (@"%@",[as executeAndReturnError:&asErrDic]);
+    return [as executeAndReturnError:&asErrDic];
+}
+
+- (IBAction)GetStringRV:(id)sender {
+    NSAppleEventDescriptor *aeDesc = [self GetAEDesc:@"test_str.scpt"];
+    NSLog(@"%@",[aeDesc stringValue]);
+}
+
+- (IBAction)GetIntegerRV:(id)sender {
+    NSAppleEventDescriptor *aeDesc = [self GetAEDesc:@"test_int.scpt"];
+    NSLog(@"%i",[aeDesc int32Value]);
+}
+
+- (IBAction)GetListRV:(id)sender {
+    NSMutableArray *array = [NSMutableArray array];
+    NSAppleEventDescriptor *aeDesc = [self GetAEDesc:@"test_list.scpt"];
+    NSInteger CntItems = [aeDesc numberOfItems];
+    for (NSInteger i = 1; i <= CntItems; i++) {
+        [array addObject:[[aeDesc descriptorAtIndex:i]stringValue]];
+    }
+    NSLog(@"%@",array);
+}
+
+- (IBAction)GetRecordRV:(id)sender {
+    NSMutableDictionary *dict = [NSMutableDictionary dictionary];
+    NSAppleEventDescriptor *aeDesc = [self GetAEDesc:@"test_rec.scpt"];
+    [dict setObject:[[aeDesc descriptorForKeyword:'pnam']stringValue] forKey:@"name"];
+    NSAppleEventDescriptor *valParam = [aeDesc descriptorForKeyword:'usrf'];
+    NSString *name = nil;
+    NSString *param = nil;
+    NSInteger CntParam = [valParam numberOfItems];
+    for (NSInteger i = 1; i <= CntParam; i++) {
+        NSAppleEventDescriptor *val = [valParam descriptorAtIndex:i];
+        NSString *str = [val stringValue];
+        if (name) {
+            [dict setObject:str forKey:name];
+            name = nil;
+            param = nil;
+        }else{
+            name = str;
+        }
+    }
+    NSLog(@"%@",dict);
 }
 
 @end
